52f7eefe6575af86d73d3b27cc2db849abb15a40
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-05-26 17:49:21 +0200

    Prepare release

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -62,7 +62,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('1.0', dirname(dirname(__DIR__)));
+        $version = new Version('1.0.0', dirname(dirname(__DIR__)));
         parent::__construct('test-mapper', $version->getVersion());
     }
 
